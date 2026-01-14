15a9bde3cf31f13f80389a9ab6b096a6553e6ef1
Josue Barrera <josBarMarLanz@gmail.com>
2026-01-14 22:45:28 +0000

    feat: Updating mapComponent
    - Deleting ununused service getLocation
    - Adding eess-card component
    - Adding some css format

--- .devcontainer/web/post-create.sh
+++ .devcontainer/web/post-create.sh
@@ -7,11 +7,6 @@ set -x
 echo "- Running post-create.sh---------------------------->"
 apt update && apt install iputils-ping -y;
 npm install -g @angular/cli;
-# npm install ngx-markdown --save
-# npm install emoji-toolkit@^9.0.0 --save;
-# npm install mermaid@^11.0.0 --save;
-# npm install katex@^0.16.0 --save
-# npm install prismjs@^1.30.0 --save;
 npm install leaflet@^1.9.4 --save;
 npm install @types/leaflet --save;
 npm install @angular/material --save;
